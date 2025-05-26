a4239db41de7994f71883a2e6a62e4a0c4b7be7e
Marcelo Alves <marcelorenato_moc@hotmail.com>
2025-05-26 00:37:33 -0300

    Basic setup

---
+++ basic/modtest.py
@@ -0,0 +1,7 @@
+"""modtest: our test example"""
+def f(x):
+    return x
+def _g(x):
+    return x
+a = 4
+_b = 2
---
+++ basic/mymath.py
@@ -0,0 +1,5 @@
+"""my math - our example math module"""
+pi = 3.14159
+def area(r):
+    """area(r): return the area of a circle with redius r."""
+    return(pi * r * r)
